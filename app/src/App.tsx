a2955d4ce4a8cf1c67fb130ba80707c6e389e9f8
Jack Song <133342301@163.com>
2021-02-06 21:54:07 +1100

    Commented out ErrorBoundary

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,33 @@
 import React from "react";
-import ErrorBoundary from "@millifx/error-boundary";
+// import ErrorBoundary from "@millifx/error-boundary";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+// const AppWithErrorBoundary = (
+//   <ErrorBoundary>
+//     <App />
+//   </ErrorBoundary>
+// )
+
+const App = () => {
   return (
-    <ErrorBoundary>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ErrorBoundary>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-}
+};
 
 export default App;
