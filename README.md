143e958ddbec448127a838ba4b4bc786260a6952
Jack Song <133342301@163.com>
2021-02-06 17:29:09 +1100

    Integrated Storybook & Snapshots

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Storybook](https://cdn.jsdelivr.net/gh/storybookjs/brand@master/badge/badge-storybook.svg)]()
---
+++ storybook/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    // "../../app/src/**/*.stories.mdx",
+    // "../../app/src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-a11y',
+  ]
+}
