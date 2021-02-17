0341beeb68cac1690b3dfe956b78061214ada235
Jack Song <133342301@163.com>
2021-02-17 15:53:06 +1100

    Added Sample Page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ErrorBoundary from "@millifx/error-boundary";
-import logo from "./logo.svg";
+import { Account } from "@millifx/utils";
+import { Breadcrumb, Layout, Menu } from "antd";
 import { InvitationForm } from "./components/InvitationForm";
+const { Header, Footer, Sider, Content } = Layout;
+
+const sampleAccount: Account = {
+  id: 5875672,
+  name: "MilliFX Master Account",
+  gain: 545.1390111241,
+  drawdown: 0.06,
+  demo: false,
+  change: 537.105380649,
+};
 
 const App = () => (
   <ErrorBoundary version={process.env.REACT_APP_COMMIT}>
@@ -10,16 +21,34 @@ const App = () => (
 );
 
 const AppWithRouter = () => {
+  const { name, gain } = sampleAccount;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <InvitationForm />
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <h2>{name}</h2>
+          <p>Gain: {gain}</p>
+          <InvitationForm />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 };
 
