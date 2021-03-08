4e83fb6a1324c51c629b207f71f0e6d08fb394fa
sunsun0312 <jessiesun0312@gmail.com>
2021-03-08 13:58:53 +1100

    add basic chart features

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -1,23 +1,56 @@
 import * as React from "react";
 import { Daily } from "../../utils/constants";
+import { DualAxes } from '@ant-design/charts';
 
 interface ChartProps {
   data: Daily[];
 }
 
 const Chart = ({ data }: ChartProps) => {
+  const equity = data.map((daily)=>{
+    return { 
+      date: daily.date,
+      value: daily.equity,
+      label: 'equity'
+    }
+  })
+  const balance = data.map(daily=>{
+    return {
+      date: daily.date,
+      value: daily.balance,
+      label: 'balance'
+    }
+  })
+  const newData = [...equity,...balance]
+  const config = {
+    data: [newData, data],
+    height: 400,
+    xField: 'date',
+    yField: ['value', 'profit'],
+    geometryOptions: [
+      {
+        geometry: 'line',
+        padding: 'auto',
+        smooth: false,
+        seriesField: 'label'
+      },
+      {
+        geometry: 'column',
+      },
+    ]
+  };
   return (
     <>
-      <p>Convert the following list to a chart</p>
-      <ul>
-        {data.map((daily) => {
+      {/* <ul>
+        {newData.map((daily)=>{
           return (
             <li>
-              {daily.date} {daily.balance} {daily.equity} {daily.profit}
+              {daily.date} {daily.value} {daily.label}
             </li>
-          );
+          )
         })}
-      </ul>
+      </ul> */}
+      <DualAxes {...config} />
     </>
   );
 };
