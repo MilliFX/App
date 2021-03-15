feef85f356aa47a219f95df976c2ba04d3344a97
danielJing <danieljing1993@gmail.com>
2021-03-15 21:43:39 +1100

    rebuild the Y-axis label

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -38,17 +38,26 @@ const XFormat = (data: string) => {
   const Day = dateArr[1];
   const Year = dateArr[2];
   // console.log(MonthENUM[parseInt(Month)])
-  const dateResult = `${MonthENUM[parseInt(Month)]} ${Day}, '${Year.slice(2,4)}`
-  // console.log(dateArr);
+  const dateResult = `${MonthENUM[parseInt(Month)]} ${Day}, '${Year.slice(2, 4)}`
   return dateResult;
 }
 
+const LeftYFormat = (data: number): string => {
+  const dataStr = data.toFixed(0);
+  return `$${dataStr.substr(0, dataStr.length - 3)}K`;
+}
+const RightYFormat = (data: number): any => {
+ return `$${data}`
+
+}
 
 const Chart = ({ data }: ChartProps) => {
 
   const [duration, setDuration] = React.useState(data)
 
-  const handleDurationChange = (days: number): void => {
+  const RenderData = data;
+
+  const handleYDurationChange = (days: number): void => {
     console.log(days);
     const start = data.length - days;
     const end = data.length - 1;
@@ -60,40 +69,30 @@ const Chart = ({ data }: ChartProps) => {
 
 
   return (
-    <>
-      {/* <p>Convert the following list to a chart</p> */}
-      {/* <ul>
-        {data.map((daily) => {
-          return (
-            <li>
-              {daily.date} {daily.balance} {daily.equity} {daily.profit}
-            </li>
-          );
-        })}
-      </ul> */}
-
-      <ResponsiveContainer width="100%" height={300}>
+    <div style={{ width: '100%', height: 500 }}>
+      <ResponsiveContainer height="99%" width="100%">
         <ComposedChart
           data={duration}
         >
           <CartesianGrid stroke="#f5f5f5" strokeDasharray="3 5" />
           <XAxis dataKey="date" scale="band" tickFormatter={XFormat} />
-          <YAxis />
-          <YAxis name="profit" yAxisId="profit" dataKey="profit" orientation="right" />
+          {/* <YAxis tickFormatter={LeftYFormat}/> */}
+          <YAxis name="1" type='number' domain={['dataMin-500', 'dataMax+500']} tickFormatter={LeftYFormat} />
+          <YAxis name="profit" yAxisId="profit" dataKey="profit" orientation="right" type='number' tickFormatter={RightYFormat} />
           <Tooltip />
           <Legend />
-          <Bar yAxisId="profit" dataKey="profit" barSize={500} fill="#A7CF93"/>
-          <Line type="monotoneY" dataKey="balance" stroke="#D85648" />
+          <Bar yAxisId="profit" dataKey="profit" barSize={500} fill="#A7CF93" />
+          <Line type="monotone" dataKey="balance" stroke="#D85648" />
           <Line type="monotone" dataKey="equity" stroke="#F7D77C" />
         </ComposedChart>
-        {/* <button>aaa</button> */}
+
       </ResponsiveContainer>
       <div style={{ display: "flex", justifyContent: "space-around", alignItems: 'center' }}>
-        <button onClick={() => handleDurationChange(7)}>7 Days</button>
-        <button onClick={() => handleDurationChange(14)}>14 Days</button>
+        <button onClick={() => handleYDurationChange(7)}>7 Days</button>
+        <button onClick={() => handleYDurationChange(14)}>14 Days</button>
       </div>
 
-    </>
+    </div>
   );
 };
 
