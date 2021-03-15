9c02320f7d823592ade572af063c1871a3bdcae8
Shaw <ruoxiao.duan@gmail.com>
2021-03-16 00:31:29 +1100

    week feature

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -6,6 +6,7 @@ import {
   FlexibleXYPlot,
   LineMarkSeries,
   VerticalBarSeries,
+  VerticalGridLines,
   HorizontalGridLines,
   DiscreteColorLegend,
 } from "react-vis";
@@ -19,6 +20,7 @@ interface ChartProps {
 }
 
 const Chart = ({ data }: ChartProps) => {
+  //data for daily
   const dailyData = data.map((daily) => ({
     ...daily,
     date: daily.date.substr(0, 5),
@@ -29,6 +31,7 @@ const Chart = ({ data }: ChartProps) => {
 
   const [state, setState] = useState(dailyData);
   const [visi, setVisi] = useState(true);
+  const [week, setWeek] = useState([]);
 
   const [hintValue, setHintValue] = useState<any>();
   const palette = [
@@ -60,18 +63,33 @@ const Chart = ({ data }: ChartProps) => {
     ticks: { stroke: palette[6] },
     text: { stroke: "none", fill: palette[7], fontWeight: 600 },
   };
+  //Data for month
+  const month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
   const collection = data.map((daily) => ({
     ...daily,
-    date: daily.date.substr(0, 2),
+    date: month[new Date(daily.date).getMonth()],
     balance: daily.balance,
     equity: daily.equity,
     profit: daily.profit,
   }));
 
-  const helper:any= {};
-  const monthData = collection.reduce((acc:any, cur:any) => {
-    let key:any = cur.date;
+  const helper: any = {};
+  const monthData = collection.reduce((acc: any, cur: any) => {
+    let key: any = cur.date;
     if (!helper[key]) {
       helper[key] = Object.assign({}, cur);
       acc.push(helper[key]);
@@ -82,6 +100,40 @@ const Chart = ({ data }: ChartProps) => {
     }
     return acc;
   }, []);
+  //Data for week
+  // let current = new Date();
+  // let today = current.toLocaleDateString('en-US',{weekday: 'long'});
+
+  const weekData = data.map((daily) => ({
+    ...daily,
+    date:
+      new Date(daily.date).toLocaleDateString("en-US", { weekday: "long" }) ==
+      "Monday"
+        ? daily.date.substr(0, 5)
+        : daily.date,
+    balance: daily.balance,
+    equity: daily.equity,
+    profit: daily.profit,
+  }));
+
+  const myAarray: any = [];
+
+  {
+    data.map((daily) => {
+      myAarray.push(
+        new Date(daily.date).toLocaleDateString("en-US", { weekday: "long" }) ==
+          "Monday"
+          ? daily.date.substr(0, 5)
+          : ""
+      );
+    });
+  }
+  const weekAarray: any = [];
+  {
+    myAarray.map((item: any) => {
+      if (item.length > 0) weekAarray.push(item);
+    });
+  }
 
   return (
     <>
@@ -91,7 +143,13 @@ const Chart = ({ data }: ChartProps) => {
         height={chartHeight}
         colorRange={palette}
       >
-        <XAxis attr="x" attrAxis="y" orientation="bottom" style={axisStyle} />
+        <XAxis
+          attr="x"
+          attrAxis="y"
+          orientation="bottom"
+          style={axisStyle}
+          tickFormat={(d: any) => (d.length > 5 ? "" : d)}
+        />
         <YAxis
           style={axisStyle}
           attr="y"
@@ -101,7 +159,12 @@ const Chart = ({ data }: ChartProps) => {
           tickFormat={(d: any) => "$" + d}
           left={10}
         />
-        <HorizontalGridLines />
+        {/* <HorizontalGridLines /> */}
+        <VerticalGridLines
+          tickValues={week}
+          left={22}
+          //tickFormat={(d: any) => (d.length==0)?'':d}
+        />
         <LineMarkSeries
           data={state.map((daily) => ({
             x: daily.date,
@@ -135,18 +198,18 @@ const Chart = ({ data }: ChartProps) => {
 
         <YAxis
           style={axisStyle}
+          yDomain={[0, Math.round(profitMax * 1.9)]}
           attr="y"
           attrAxis="x"
           orientation="left"
-          yDomain={[0, Math.round(profitMax * 1.9)]}
           tickTotal={5}
           tickFormat={(d: any) => "$" + d}
-          left={chartWidth*0.95}
+          left={chartWidth * 0.95}
         />
 
         {hintValue ? (
           <Hint value={hintValue}>
-            <div style={{ background: palette[5], color: palette[2]}}>
+            <div style={{ background: palette[5], color: palette[2] }}>
               <p>
                 Date:{hintValue.x}
                 <br />
@@ -182,11 +245,22 @@ const Chart = ({ data }: ChartProps) => {
         >
           Monthly
         </button>
+        <button
+          onClick={() => {
+            setState(weekData);
+            setVisi(!visi);
+            setWeek(weekAarray);
+          }}
+          style={{ visibility: visi ? "visible" : "hidden" }}
+        >
+          weekly
+        </button>
         <br />
         <button
           onClick={() => {
             setState(dailyData);
             setVisi(!visi);
+            setWeek([]);
           }}
           style={{ visibility: visi ? "hidden" : "visible" }}
         >
