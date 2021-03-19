42327a194fcd994de3228977296aef0b0abd2829
Weikun Ye <weikunye1225@gmail.com>
2021-03-20 10:53:39 +1100

    chart

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -1,23 +1,94 @@
 import * as React from "react";
 import { Daily } from "../../utils/constants";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryScatter,
+} from "victory";
 
 interface ChartProps {
   data: Daily[];
 }
 
 const Chart = ({ data }: ChartProps) => {
+  const profit = data.map(({ profit }) => profit);
+
+  const labelRatio = Math.max(...profit) / 6;
+
   return (
     <>
-      <p>Convert the following list to a chart</p>
-      <ul>
-        {data.map((daily) => {
-          return (
-            <li>
-              {daily.date} {daily.balance} {daily.equity} {daily.profit}
-            </li>
-          );
-        })}
-      </ul>
+      <VictoryChart>
+        <VictoryBar
+          labelComponent={<VictoryTooltip />}
+          barWidth={5}
+          style={{ data: { fill: "#a7ce93" } }}
+          data={data}
+          x="date"
+          y={(data) => data.profit * 200}
+          labels={(data) => "$" + data.datum.profit.toFixed(2)}
+        />
+        <VictoryLine
+          style={{ data: { stroke: "#de7165", strokeWidth: 1 } }}
+          data={data}
+          x="date"
+          y="balance"
+        />
+        <VictoryScatter
+          labelComponent={<VictoryTooltip />}
+          style={{ data: { fill: "#de7165" } }}
+          size={2}
+          data={data}
+          x="date"
+          y="balance"
+          labels={(data) => "$" + data.datum.balance.toFixed(2)}
+        />
+        <VictoryLine
+          style={{ data: { stroke: "#f8df93", strokeWidth: 1 } }}
+          data={data}
+          x="date"
+          y="equity"
+        />
+        <VictoryScatter
+          labelComponent={<VictoryTooltip />}
+          style={{ data: { fill: "#f8df93" } }}
+          size={2}
+          data={data}
+          x="date"
+          y="equity"
+          labels={(data) => "$" + data.datum.equity.toFixed(2)}
+        />
+        {/* X axis */}
+        <VictoryAxis
+          style={{ tickLabels: { fontSize: 5 } }}
+          standalone={false}
+          tickValues={data.map(({ date }) => [date])}
+          tickFormat={(date) => {
+            const d = new Date(date);
+            if (d.getDay() === 1) {
+              return d.toISOString().split("T")[0];
+            } else {
+              return "";
+            }
+          }}
+        />
+
+        {/* Y axis */}
+        <VictoryAxis
+          dependentAxis
+          orientation="left"
+          style={{ tickLabels: { fontSize: 5 } }}
+          standalone={false}
+        />
+        {/* Y Axis */}
+        <VictoryAxis
+          dependentAxis
+          style={{ tickLabels: { fontSize: 5 } }}
+          orientation="right"
+        />
+      </VictoryChart>
     </>
   );
 };
