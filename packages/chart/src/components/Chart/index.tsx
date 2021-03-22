00097a776bfdc62f2df264a3880f6a7c148bf73b
sunsun0312 <jessiesun0312@gmail.com>
2021-03-22 20:08:33 +1100

    Add week/month data

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -1,147 +1,183 @@
 import * as React from "react";
 import { Daily } from "../../utils/constants";
-import { DualAxes } from '@ant-design/charts';
+import { DualAxes } from "@ant-design/charts";
 
 interface ChartProps {
   data: Daily[];
 }
 
 interface GroupData {
-    date: string;
-    value: number;
-    label: string;
+  date: string;
+  value: number;
+  label: string;
 }
 
 const month: string[] = [
-  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-]
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const Chart = ({ data }: ChartProps) => {
-  const [newData, setNewdata] = React.useState(data)
-  
+  const [newData, setNewdata] = React.useState(data);
+
   // set Data according to duration
   const handleDuration = (duration: string) => {
-    if (duration === 'Daily') {
-      setNewdata(data)
-    } else if (duration === 'Week') {
+    if (duration === "Daily") {
+      setNewdata(data);
+    } else if (duration === "Week") {
       setNewdata(weekData);
     } else {
-      setNewdata(monthData)
+      setNewdata(monthData);
     }
-  }
+  };
 
-  var maxColumn: number = data[0].profit
+  var maxColumn: number = data[0].profit;
 
   // extract week data from daily data
-  const weekData = data.filter((daily:Daily)=>{
-    return new Date(daily.date).getDay() === 1
-  })
+  const weekData = data.filter((daily: Daily) => {
+    return new Date(daily.date).getDay() === 1;
+  });
 
   //extract month data from weekData
-  const monthData = weekData.reduce((arr, b)=> {
-    const found = arr.find(a=>a.date.slice(0,2) === b.date.slice(0,2))
-    if (!found) {
-      arr.push(b)
-    }
-    return arr
-  },[weekData[0]])
+  const monthData = weekData.reduce(
+    (arr, b) => {
+      const found = arr.find((a) => a.date.slice(0, 2) === b.date.slice(0, 2));
+      if (!found) {
+        arr.push(b);
+      }
+      return arr;
+    },
+    [weekData[0]]
+  );
 
   // extract equity
-  const equity = newData.map((daily: Daily): GroupData=>{
-    if (daily.profit > maxColumn) {
-      maxColumn = daily.profit
+  const equity = newData.map(
+    (daily: Daily): GroupData => {
+      if (daily.profit > maxColumn) {
+        maxColumn = daily.profit;
+      }
+      return {
+        date: daily.date,
+        value: daily.equity,
+        label: "equity",
+      };
     }
-    return { 
-      date: daily.date,
-      value: daily.equity,
-      label: 'equity'
-    }
-  })
+  );
 
   // extract balance data
-  const balance = newData.map((daily:Daily):GroupData=>{
-    return {
-      date: daily.date,
-      value: daily.balance,
-      label: 'balance'
+  const balance = newData.map(
+    (daily: Daily): GroupData => {
+      return {
+        date: daily.date,
+        value: daily.balance,
+        label: "balance",
+      };
     }
-  })
+  );
 
   // merge value
-  const yValue = [...equity,...balance]
+  const yValue = [...equity, ...balance];
   const config = {
     data: [newData, yValue],
     appendPadding: 30,
-    xField: 'date',
-    yField: ['profit','value'],
-
+    xField: "date",
+    yField: ["profit", "value"],
     geometryOptions: [
       {
-        geometry: 'column',
+        geometry: "column",
         maxColumnWidth: 20,
       },
       {
-        geometry: 'line',
+        geometry: "line",
         smooth: false,
-        seriesField: 'label',
-        color: ['#E85E5E', '#E8C55E'],
-        
+        seriesField: "label",
+        color: ["#E85E5E", "#E8C55E"],
+
         point: {
-          shape: 'circle',
+          shape: "circle",
           size: 4.5,
         },
-        
       },
     ],
     xAxis: {
-
-      grid: { line: {
-        style: { stroke: '#eee' }
-      }},
-      
+      type: "timeCat",
+      grid: {
+        line: {
+          style: { stroke: "#eee" },
+        },
+      },
     },
     yAxis: [
-        {
+      {
         min: -maxColumn,
         tickCount: 10,
-      }
-      
+      },
     ],
     meta: {
       date: {
-        formatter: function formatter(d:string) {
-          const date = new Date(d)
-          return `${month[date.getMonth()]} ${date.getDate()}, '${date.getFullYear().toString().slice(2,4)}`
+        formatter: function formatter(d: string) {
+          const date = new Date(d);
+          return `${
+            month[date.getMonth()]
+          } ${date.getDate()}, '${date.getFullYear().toString().slice(2, 4)}`;
           // return `${month[parseInt(date[0])-1]} ${date[1]}, '${date[2]}`
-        }
-      }
-    }, 
+        },
+      },
+    },
     legend: {
-      position: 'bottom',
+      position: "bottom" as "bottom",
       // layout: 'horizontal',
     },
     tooltip: {
-        showCrosshairs: true,
+      showCrosshairs: true,
       crosshairs: {
-        type: 'x',
+        type: "x" as "x",
       },
       showMarkers: true,
       marker: {
-        symbol: 'circle'
-      }
+        symbol: "circle",
+      },
     },
-    
+    // interactions: [{ type: "marker-active" }, { type: "brush" }],
   };
+
   return (
     <>
-    {/* {console.log(newData)} */}
-      <div className="row" style={{ textAlign: 'center' }}>
-        <button  type="button" style={{ margin: '10px' }} onClick={()=>handleDuration('Daily')}>1 Day</button>
-        <button  type="button" style={{ margin: '10px' }} onClick={()=>handleDuration('Week')}>1 week</button>
-        <button  type="button" style={{ margin: '10px' }} onClick={()=>handleDuration('Month')}>1 month</button>
+      {/* {console.log(newData)} */}
+      <div className="row" style={{ textAlign: "center" }}>
+        <button
+          type="button"
+          style={{ margin: "10px" }}
+          onClick={() => handleDuration("Daily")}
+        >
+          Day
+        </button>
+        <button
+          type="button"
+          style={{ margin: "10px" }}
+          onClick={() => handleDuration("Week")}
+        >
+          Week
+        </button>
+        <button
+          type="button"
+          style={{ margin: "10px" }}
+          onClick={() => handleDuration("Month")}
+        >
+          Month
+        </button>
       </div>
-      
+
       <DualAxes {...config} />
     </>
   );
