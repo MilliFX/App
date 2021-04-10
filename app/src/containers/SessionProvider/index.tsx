9adccb6818d9d5de749635edba9362ca33a59763
danielJing <danieljing1993@gmail.com>
2021-04-10 17:27:59 +1000

    add uuid localstorage check

--- app/src/containers/SessionProvider/index.tsx
+++ app/src/containers/SessionProvider/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { SessionContext } from "./context";
 import { initialState, SessionState } from "./state";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { v1 as uuidv1 } from "uuid";
 
 export const SessionProvider: React.FC = ({ children }) => {
   const now = Date();
   const [session, setSession] = useState<SessionState>(initialState);
+
   // TODO: AP-118
+  useEffect(() => {
+    if (!localStorage.getItem("uuid")) {
+      const uuid: string = uuidv1();
+      localStorage.setItem("uuid", uuid);
+    }
+    console.log(localStorage.getItem("uuid"));
+  }, []);
   // TODO: AP-119
   // TODO: AP-120
 
