ab99106ea71187a75a1ec18b67ae452ca2d995e1
Jack Song <133342301@163.com>
2021-04-12 20:01:39 +1000

    Added custom hook

--- app/src/containers/SessionProvider/index.tsx
+++ app/src/containers/SessionProvider/index.tsx
@@ -2,23 +2,16 @@ import * as React from "react";
 import { SessionContext } from "./context";
 import { initialState, SessionState } from "./state";
 import { useState } from "react";
-import { v1 as uuidv1 } from "uuid";
+import { useUUID } from "./useUUID";
 
 export const SessionProvider: React.FC = ({ children }) => {
-  const now = Date();
-  const [session, setSession] = useState<SessionState>(initialState);
-  // TODO: AP-118
-  // TODO: AP-119
-  const uuidGenerator = () => {
-    // localStorage.removeItem("uuid");
-    if (!localStorage.uuid) {
-      const uuid = uuidv1();
-      localStorage.setItem("uuid", uuid);
-      setSession({ uuid: localStorage.uuid });
-    }
-  };
-  uuidGenerator();
-  // TODO: AP-120
+  const uuid = useUUID();
+
+  const [session] = useState<SessionState>({
+    ...initialState,
+    uuid: uuid,
+  });
+
   return (
     <SessionContext.Provider value={session}>
       {children}
