833d7076afabdf32902bc8118794a63649fed1ee
Weikun Ye <weikunye1225@gmail.com>
2021-04-19 10:08:14 +1000

    specific header and domain control

--- functions/src/hello/index.ts
+++ functions/src/hello/index.ts
@@ -1,4 +1,5 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { headerValidation } from "../util/headerValidation";
 
 export interface HelloResponse {
   message: string;
@@ -9,14 +10,36 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   // your server-side functionality
 
-  const msg = event.queryStringParameters?.msg;
-  const response: HelloResponse = { message: "Hello World " + msg };
+  //console.log(event.headers);
 
+  //Initial status code of 200
+  var statusCode: number = 200;
+
+  //Initial response
+  var response: HelloResponse = { message: ""};
+
+  if(headerValidation(event.headers)){
+
+    //If specific header valid, prepare response
+    const msg = event.queryStringParameters?.msg;
+    response.message = "Hello World " + msg;
+
+  }else{
+
+    //If specific header invalid, change status code to 401 and prepare response
+    statusCode = 401;
+    response.message = "Unauthorized request";
+
+  }
+
+  //response:
   return {
-    statusCode: 200,
+    statusCode: statusCode,
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "https://app-dev.millifx.com",
     },
     body: JSON.stringify(response),
   };
+
 };
