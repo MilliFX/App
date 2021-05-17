34663927e12e07895e55db8107c72a11c46c8eee
Tiange-Lei <encore0722@gmail.com>
2021-05-17 19:47:37 +1000

    Completed

--- packages/skeleton/src/components/Skeleton/index.stories.tsx
+++ packages/skeleton/src/components/Skeleton/index.stories.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { default as Component } from ".";
-import { sample } from '../../utils/sample'
 export default {
     title: "@millifx/skeleton",
     component: Component,
   };
 
-export const Skeleton = () =><Component data={sample} active={true} loading={true}/>
+export const Skeleton = ()=>{
+  return(
+    <Component active={true} loading={true}/>
+  ) 
+}
--- packages/skeleton/src/components/Skeleton/index.tsx
+++ packages/skeleton/src/components/Skeleton/index.tsx
@@ -1,24 +1,10 @@
 import * as React from "react";
-import { Skeleton } from 'antd';
+import { Skeleton as AntSkeleton, SkeletonProps } from 'antd';
 
-interface SkeletonProps{
-    data:string
-    active:boolean
-    loading:boolean
-}
-
-const MilliSkeleton = ({loading,active}:SkeletonProps) => {
+const Skeleton = (props: SkeletonProps) => {
     return(
-        <div>
-            <Skeleton active={active} loading={loading}>
-                something
-                something
-                something
-                something
-                something
-            </Skeleton>
-        </div>
+            <AntSkeleton {...props} />
     )
 }
 
-export default MilliSkeleton;
+export default Skeleton;
