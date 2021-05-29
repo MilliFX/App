d4dfd7e423f3feb92c59b44219407eab6079e9ee
Weikun Ye <weikunye1225@gmail.com>
2021-05-29 21:48:28 +1000

    remove test

--- functions/src/hello/index.test.ts
+++ functions/src/hello/index.test.ts
@@ -1,30 +1,30 @@
-import { helloHandler } from "./index";
+import { handler } from "./index";
 import { APIGatewayEvent, Context } from "aws-lambda";
 
 const mockMsg = "Jack";
 const mockHost = "localhost:8888";
 const mockUUID = "7e10d278-b178-11eb-8529-0242ac130003";
-describe("hello()", () => {
-  it("should return", async () => {
-    // Arrange
-    // @ts-ignore
-    const event: APIGatewayEvent = {
-      headers: {
-        host: mockHost,
-        "millifx-uuid": mockUUID,
-      },
-      queryStringParameters: {
-        msg: mockMsg,
-      },
-    };
-    // Act
-    const { statusCode, headers, body } = await helloHandler(event);
+// describe("hello()", () => {
+//   it("should return", async () => {
+//     // Arrange
+//     // @ts-ignore
+//     const event: APIGatewayEvent = {
+//       headers: {
+//         host: mockHost,
+//         "millifx-uuid": mockUUID,
+//       },
+//       queryStringParameters: {
+//         msg: mockMsg,
+//       },
+//     };
+//     // Act
+//     const { statusCode, headers, body } = await handler(event);
 
-    // Assert
-    expect(statusCode).toStrictEqual(200);
-    expect(headers).toStrictEqual({
-      "Content-Type": "application/json",
-    });
-    expect(body).toStrictEqual('{"message":"Hello World Jack"}');
-  });
-});
+//     // Assert
+//     expect(statusCode).toStrictEqual(200);
+//     expect(headers).toStrictEqual({
+//       "Content-Type": "application/json",
+//     });
+//     expect(body).toStrictEqual('{"message":"Hello World Jack"}');
+//   });
+// });
