c950fcab271e96a166e03ac6e739bb474840641d
WeikunYe <weikunye1225@gmail.com>
2021-07-26 20:47:02 +1000

    build html structure and demo data

--- app/src/components/Link/index.stories.tsx
+++ app/src/components/Link/index.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Typography } from 'antd'
-
+import { Typography } from "antd";
 
 const Component = Typography.Link;
 export default {
@@ -9,22 +8,16 @@ export default {
   argTypes: {
     children: { control: "text" },
     disabled: {
-      control: { type: "select", options: { Yes: true, No: false } }
-    }
-  }
+      control: { type: "select", options: { Yes: true, No: false } },
+    },
+  },
 };
 
-
-const Template = (args) => (
-  <Component
-    {...args}
-    disabled={args.disabled}
-  />
-);
+const Template = (args) => <Component {...args} disabled={args.disabled} />;
 
 export const Link = Template.bind({});
 Link.args = {
-  children: 'A demo link',
+  children: "A demo link",
   disabled: false,
-  underline: false
-}
+  underline: false,
+};
