0adee4094e56c518f0b8e3a0c1924a34df6d5fe9
WeikunYe <weikunye1225@gmail.com>
2021-07-27 17:59:38 +1000

    add style

---
+++ app/src/pages/transactions/SingleDayRow.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { ITransection } from "./SingleTransectionRow";
+import { default as TransectionRow } from "./SingleTransectionRow";
+import { StyleDateTitle, StyleDateWrapper } from "./styles";
+
+export interface ITransections {
+  date: string;
+  transections: ITransection[];
+}
+
+interface IDateRowProps {
+  data: ITransections;
+}
+
+const DateRow = ({ data }: IDateRowProps) => {
+  return (
+    <>
+      <StyleDateWrapper>
+        <StyleDateTitle>{data.date}</StyleDateTitle>
+        {data.transections.map((transection, key) => (
+          <TransectionRow transection={transection} key={key} />
+        ))}
+      </StyleDateWrapper>
+    </>
+  );
+};
+
+export default DateRow;
