7bc3a95bb489dde2cfe908cb43a298e0ff2b39e9
WeikunYe <weikunye1225@gmail.com>
2021-08-13 23:02:21 +1000

    build frontend transection api and fix backend transection bugs

--- app/src/pages/transactions/index.tsx
+++ app/src/pages/transactions/index.tsx
@@ -10,28 +10,57 @@ import {
   StyleHeroDataTitle,
   StyleHeroDataFigure,
 } from "./styles";
-
 import { formatCurrency } from "../../utils/formatCurrency";
+import { TransectionHandlerResponse } from "@millifx/utils";
+import { useState, useEffect } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import { UUID_FIELD } from "../../utils/constants";
 
 const Transactions = () => {
-  return (
-    <Row>
-      <Col span={24}>
-        <StyleHeroWrapper>
-          <StyleHeroTitle>Transections</StyleHeroTitle>
-          <StyleHeroDataWrapper>
-            <StyleHeroDataTitle>Daily Profit</StyleHeroDataTitle>
-            <StyleHeroDataFigure profit={-155.56}>
-              {formatCurrency(-155.56)}
-            </StyleHeroDataFigure>
-          </StyleHeroDataWrapper>
-        </StyleHeroWrapper>
-        {transectionData.map((date, i) => (
-          <DayRow data={date} key={i} />
-        ))}
-      </Col>
-    </Row>
-  );
+  const [data, setData] = useState<TransectionHandlerResponse>();
+
+  const fetchTransectionData = async () => {
+    try {
+      const axiosConfig: AxiosRequestConfig = {
+        method: "get",
+        url: "http://localhost:3000/api/transections",
+        headers: {
+          "millifx-uuid": localStorage[UUID_FIELD],
+        },
+      };
+      const res = await axios(axiosConfig);
+      setData(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransectionData();
+  }, []);
+
+  if (data && data.data !== null && !data.error) {
+    return (
+      <Row>
+        <Col span={24}>
+          <StyleHeroWrapper>
+            <StyleHeroTitle>Transections</StyleHeroTitle>
+            <StyleHeroDataWrapper>
+              <StyleHeroDataTitle>Daily Profit</StyleHeroDataTitle>
+              <StyleHeroDataFigure profit={-155.56}>
+                {formatCurrency(-155.56)}
+              </StyleHeroDataFigure>
+            </StyleHeroDataWrapper>
+          </StyleHeroWrapper>
+          {data.data.history.map((date, i) => (
+            <DayRow data={date} key={i} />
+          ))}
+        </Col>
+      </Row>
+    );
+  } else {
+    return <div>show error component</div>;
+  }
 };
 
 export default Transactions;
--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const FULLSTORY_ORG_ID = "12NQHV";
+export const UUID_FIELD = "uuid";
