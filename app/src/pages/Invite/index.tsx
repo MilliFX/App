ca5110c8aabdcf6355e4d8ee73698616ba2b4056
danielJing <danieljing1993@gmail.com>
2021-08-18 21:00:23 +1000

    merge conflicts

--- app/src/pages/Invite/index.tsx
+++ app/src/pages/Invite/index.tsx
@@ -25,3 +25,49 @@ export const Invite = () => {
     </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 320px;
+  height: auto;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InputWrapper = styled.div`
+  width: 320px;
+  height: auto;
+  margin-bottom: 20px;
+  display: flex;
+  flex-diretion: column;
+  align-items: flex-start;
+  justify-content: center;
+  border: 1px solid green;
+`;
+
+const InviteForm = () => {
+  return (
+    <>
+      <Form>
+        <Form.Item
+          label="Enter Invitation Code"
+          hasFeedback
+          validateStatus="warning"
+          help="Please enter your invitation code"
+        >
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" block>
+            Submit
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Button block> Cancel </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
