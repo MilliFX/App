a5218c8c4c939cb05929dc45a184cfc2a64139d6
danielJing <danieljing1993@gmail.com>
2021-08-18 22:53:47 +1000

    add other attributes for demoAccount

--- functions/src/v1/mgmt/trading/demo/types.ts
+++ functions/src/v1/mgmt/trading/demo/types.ts
@@ -4,7 +4,18 @@ export interface Income {
 }
 
 export interface DemoAccount {
-  account: string;
+  account: number;
+  name: string;
+  description: string;
+  withdrawals: number;
+  gain: number;
+  absGain: number;
   balance: number;
   equity: number;
+  monthly: number;
+  drawdown: number;
+  currency: string;
+  profitFactor: number;
+  creationDate: string;
+  lastUpdateDate: string;
 }
