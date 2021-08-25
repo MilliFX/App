ca7b91ee83d555d7919cdb329ec59814d5f16c89
WeikunYe <weikunye1225@gmail.com>
2021-08-25 14:59:58 +1000

    create by month and by week

--- packages/chart/src/components/Chart/index.tsx
+++ packages/chart/src/components/Chart/index.tsx
@@ -3,12 +3,18 @@ import { Daily } from "../../utils/constants";
 import AChart from "react-apexcharts";
 import { bankersRound } from "bankers-round";
 import moment from "moment";
+import { useState } from "react";
 
 interface ChartProps {
   data: Daily[];
 }
 
-const Chart = ({ data }: ChartProps) => {
+enum Duration {
+  week= "week",
+  month = "month"
+}
+
+const Chart: React.FC<ChartProps> = ({ data }: ChartProps) => {
   const getXaxis = (data: Daily[]) => {
     const xaxis = data.map((item) => moment(item.date).format("DD MMM"));
     return xaxis;
@@ -17,15 +23,11 @@ const Chart = ({ data }: ChartProps) => {
     const balances = data.map((item) => bankersRound(item.balance, 2));
     return balances;
   };
-  const getProfit = (data: Daily[]) => {
-    const profits = data.map((item) => bankersRound(item.profit, 2));
-    return profits;
-  };
   const getEquity = (data: Daily[]) => {
     const equities = data.map((item) => bankersRound(item.equity, 2));
     return equities;
   };
-  const options = {
+  const [options, setOptions] = useState({
     chart: {
       id: "basic-line",
     },
@@ -38,22 +40,99 @@ const Chart = ({ data }: ChartProps) => {
     xaxis: {
       categories: getXaxis(data),
     },
-  };
-  const series = [
+  });
+  const [series, setSeries] = useState([
     {
       name: "Balance",
       data: getBalance(data),
     },
-    { name: "Equity", data: getEquity(data) },
-  ];
+    { 
+      name: "Equity", 
+      data: getEquity(data) },
+  ]);
+
+  const byPeriod = (duration: Duration) => {
+    const lastDay =  moment(data[data.length-1].date);
+    switch (duration) {
+      case "week":
+        const dayOfWeek = lastDay.day();
+        const lastMondayOfWeek = lastDay.subtract(dayOfWeek-1, "days");
+        const weeklyData = data.filter((item) => {
+          const day = moment(item.date);
+          if(day.diff(lastMondayOfWeek) >= 0){
+            return item;
+          }
+        });
+        setOptions({
+          chart: {
+            id: "basic-line",
+          },
+          colors: ["#E71F18", "#3FADEC"],
+          yaxis: {
+            labels: {
+              show: false,
+            },
+          },
+          xaxis: {
+            categories: getXaxis(weeklyData),
+          },
+        });
+        setSeries([
+          {
+            name: "Balance",
+            data: getBalance(weeklyData),
+          },
+          { 
+            name: "Equity", 
+            data: getEquity(weeklyData) },
+        ]);
+        break;
+      case "month":
+        const startOfMonth  = lastDay.startOf("month");
+        console.log(startOfMonth);
+        const monthlyData = data.filter((item) => {
+          const day = moment(item.date);
+          if(day.diff(startOfMonth) >= 0){
+            return item;
+          }
+        });
+        setOptions({
+          chart: {
+            id: "basic-line",
+          },
+          colors: ["#E71F18", "#3FADEC"],
+          yaxis: {
+            labels: {
+              show: false,
+            },
+          },
+          xaxis: {
+            categories: getXaxis(monthlyData),
+          },
+        });
+        setSeries([
+          {
+            name: "Balance",
+            data: getBalance(monthlyData),
+          },
+          { 
+            name: "Equity", 
+            data: getEquity(monthlyData) },
+        ]);
+        break;
+    }
+  }
   return (
-    <AChart
-      options={options}
-      series={series}
-      type="line"
-      width="100%"
-      height="600"
-    />
+    <>
+      <AChart
+        options={options}
+        series={series}
+        type="line"
+        width="100%"
+        height="500"
+      />
+      <button onClick={() => byPeriod(Duration.week)}>W</button><button onClick={() => byPeriod(Duration.month)}>M</button>
+    </>
   );
 };
 
