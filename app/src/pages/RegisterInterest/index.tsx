fd2d83d99d957225a4b482173a23e44b5aaa107d
danielJing <danieljing1993@gmail.com>
2021-09-03 22:27:26 +1000

    change the layout, set width to 320px

--- app/src/pages/RegisterInterest/index.tsx
+++ app/src/pages/RegisterInterest/index.tsx
@@ -5,24 +5,27 @@ import {
   HeaderTitle,
   HeaderWithCloseButton,
   RegisterInterestDescription,
+  OutWrapper,
 } from "./index.style";
 import { CloseOutlined } from "@ant-design/icons";
 import RegisterForm from "../../components/RegisterForm";
 
 const RegisterInterest: React.FC = () => {
   return (
-    <Wrapper>
-      <HeaderWithCloseButton>
-        <HeaderTitle>Register your interest</HeaderTitle>
-        <CloseButton href="/more">
-          <CloseOutlined />
-        </CloseButton>
-      </HeaderWithCloseButton>
-      <RegisterInterestDescription>
-        Sign up to be notified of any development updates and future release.
-      </RegisterInterestDescription>
-      <RegisterForm />
-    </Wrapper>
+    <OutWrapper>
+      <Wrapper>
+        <HeaderWithCloseButton>
+          <HeaderTitle>Register your interest</HeaderTitle>
+          <CloseButton href="/more">
+            <CloseOutlined />
+          </CloseButton>
+        </HeaderWithCloseButton>
+        <RegisterInterestDescription>
+          Sign up to be notified of any development updates and future release.
+        </RegisterInterestDescription>
+        <RegisterForm />
+      </Wrapper>
+    </OutWrapper>
   );
 };
 
