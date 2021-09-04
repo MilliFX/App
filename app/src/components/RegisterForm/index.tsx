17abb3b2a641d342830f0066978a9659983d1826
danielJing <danieljing1993@gmail.com>
2021-09-04 12:52:11 +1000

    set register form input and button active after close Modal

--- app/src/components/RegisterForm/index.tsx
+++ app/src/components/RegisterForm/index.tsx
@@ -9,23 +9,16 @@ import {
 } from "../../pages/Invite/index.style";
 import { FormWrapper } from "../../pages/RegisterInterest/index.style";
 import { Button } from "../Button/index.stories";
-import StyleModal from "./StyleModal";
 
 const RegisterForm: React.FC = () => {
   const [formWarning, setFormWarning] = useState<boolean>(false);
   const [inputDisabled, setInputDisabled] = useState<boolean>(false);
   const [btnLoading, setBtnLoading] = useState<boolean>(false);
-  const [isModalRender, setIsModalRender] = useState<boolean>(false);
   const [registerEmail, setRegisterEmail] = useState<
     string | number | undefined
   >(undefined);
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRegisterEmail(e.target.value);
-  };
-  const handleModalOk = () => {
-    setInputDisabled(!inputDisabled);
-    setBtnLoading(!btnLoading);
-    setIsModalRender(false);
   };
   const onCheck = () => {
     if (!registerEmail) {
@@ -34,11 +27,25 @@ const RegisterForm: React.FC = () => {
       setInputDisabled(!inputDisabled);
       setBtnLoading(!btnLoading);
       /*
-         call api success code
+         email validation and call api success code
       */
-      setIsModalRender(!isModalRender);
+      Modal.success(modalConfig);
       //call api return failed code
     }
+  };
+  const modalConfig = {
+    title: "Thank you for registering your interest",
+    content: (
+      <ModalMessage>
+        We will send you an update when MilliFX is open for registration.
+      </ModalMessage>
+    ),
+    onButtonProps: { size: "small" },
+    afterClose: () => {
+      setInputDisabled(false);
+      setBtnLoading(false);
+      setFormWarning(false);
+    },
   };
   return (
     <FormWrapper>
@@ -62,21 +69,6 @@ const RegisterForm: React.FC = () => {
       <Button type="primary" block onClick={onCheck} loading={btnLoading}>
         Submit
       </Button>
-      <Modal
-        title="Thank you for registering your interest"
-        closable={false}
-        visible={isModalRender}
-        modalRender={undefined}
-        footer={[
-          <Button type="primary" onClick={handleModalOk}>
-            OK
-          </Button>,
-        ]}
-      >
-        <ModalMessage>
-          We will send you an update when MilliFX is open for registration.
-        </ModalMessage>
-      </Modal>
     </FormWrapper>
   );
 };
