4e664701d6a0b08b1ea26d08656c8e4c56034f33
danielJing <danieljing1993@gmail.com>
2021-09-05 14:54:56 +1000

    rebase from development after merging AP-158

--- app/src/pages/More/index.tsx
+++ app/src/pages/More/index.tsx
@@ -24,7 +24,7 @@ const More: React.FC = () => {
       <BottomWrapper>
         <Navigation>
           <NavigationItem>
-            <NavigationLink href="/">
+            <NavigationLink href="/registerinterest">
               <AlertOutlined />
               <span>Register your interest</span>
             </NavigationLink>
